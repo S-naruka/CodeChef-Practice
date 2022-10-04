bd859eff386512e199bb8154612281f734cc5de1
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2022-10-04 15:46:01 +0530

    Create STICKBREAK.cpp

---
+++ STICKBREAK.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	  cin>>t;
+	    while(t--){
+        int l,k;
+	      cin>>l>>k;
+	   
+	      if((l%k)==0)
+	        cout<<"0"<<endl;
+	      else
+	        cout<<"1"<<endl;
+	   
+     }
+	return 0;
+}
