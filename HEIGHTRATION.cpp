5051295d413e1a288a79f6f92cb318071924b990
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2022-10-04 15:57:25 +0530

    Create HEIGHTRATION.cpp

---
+++ HEIGHTRATION.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	  int a,b;
+	    cin>>a>>b;
+	
+        if(a<b)
+	        cout<<b;
+	
+        if(b<a)
+          cout<<a;
+	
+  return 0;
+}
