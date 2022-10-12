e0483bf99bd2d4853699e762d370a25cd4d54e3c
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2022-10-13 00:46:38 +0530

    Create TALLER.cpp

---
+++ TALLER.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{ int a,b;
+	    cin>>a>>b;
+	    
+	    if(a<b)
+	        cout<<"B"<<endl;
+	    else
+	        cout<<"A"<<endl;
+	   
+	   
+	}
+	return 0;
+}
